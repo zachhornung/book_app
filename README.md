8d5bc1fd021608c7f4e01f7a33b79f6dceb60b0a
Zach Hornung <Zachhornung@comcast.net>
2021-03-01 14:56:40 -0800

    basic setup achieved

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # book_app
+
+Number and name of feature: getting routes set up for book search, and css to link.
+
+Estimated time needed to complete: 6 hrs
+
+start time: 2:32pm
+
+Finish time:
+
+actual time needed to complete:
---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const superagent = require('superagent');
+
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  const ejsObject = {teacher: 'nicholas', course: '301d71'};
+  res.render('pages/index.ejs', ejsObject);
+});
+
+app.get('/hello', (req, res) => {
+  const ejsObject = {teacher: 'nicholas', course: '301d71'};
+  res.render('pages/index.ejs', ejsObject)
+});
+
+
+
+app.listen(PORT, () => console.log(`up on http://localhost:${PORT}`));
